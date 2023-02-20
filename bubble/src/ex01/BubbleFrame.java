eac84d6e602eb5d52e9a1b54ad99e1ccd6013751
questzz <questzz@naver.com>
2023-02-20 11:21:30 +0900

    버플프레임 player 초기 코드

---
+++ bubble/src/ex01/BubbleFrame.java
@@ -0,0 +1,66 @@
+package ex01;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class BubbleFrame extends JFrame {
+
+	private JLabel backgroundMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initData();
+		setInitLayout();
+		addEventListener();
+	}
+
+	private void initData() {
+		backgroundMap = new JLabel(new ImageIcon("images/backgroundMap.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(backgroundMap);
+		setSize(1000, 640);
+
+		player = new Player();
+	}
+
+	private void setInitLayout() {
+		setLayout(null); // 좌표 기반
+		setResizable(false);
+		setLocationRelativeTo(null); // JFrame 가운데 배치
+		setVisible(true);
+
+		add(player);
+	}
+
+	private void addEventListener() {
+		this.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.right();
+					break; // 세미콜론
+				case KeyEvent.VK_UP:
+					player.up();
+					break;
+				case KeyEvent.VK_DOWN:
+					player.down();
+					break;
+				}
+			}
+		});
+
+	}
+
+	public static void main(String[] args) {
+		new BubbleFrame();
+	} // end of main
+} // end of class
