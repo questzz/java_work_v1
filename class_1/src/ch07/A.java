1a3ed28af9bdfa062df26bd424ad7c990457c0ee
questzz <questzz@naver.com>
2023-02-21 14:23:38 +0900

    bubble 클래스 생성

---
+++ class_1/src/ch07/A.java
@@ -0,0 +1,10 @@
+package ch07;
+
+public class A {
+	
+	B b;
+	public A() {
+		b = new B();
+	}
+	
+}
---
+++ class_1/src/ch07/Main.java
@@ -0,0 +1,10 @@
+package ch07;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		A a = new A();
+		// 문제 
+		// d object 에 있는 value 값을 화면에 출력 해주세요 !
+	}
+}
